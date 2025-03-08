c6991b0525669306d2060bc7d347d35c7fa962b2
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-08 16:13:38 +0700

    pagination  api-side  booking feature done

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -57,13 +58,10 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 10;
-
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const currentPage = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
-
   const pageCount = Math.ceil(count / PAGE_SIZE);
 
   const nextPage = () => {
