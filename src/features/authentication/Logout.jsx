bd0911d6846d83b2caad539fd52b54c3be19710e
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-10 23:31:39 +0700

    user logout done

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,15 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import { useLogout } from "./useLogout";
+import ButtonIcon from "../../ui/ButtonIcon";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+function Logout() {
+  const { isLoggingOut, logout } = useLogout();
+  return (
+    <ButtonIcon disabled={isLoggingOut} onClick={logout}>
+      {!isLoggingOut ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
